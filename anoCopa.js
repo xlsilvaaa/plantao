61fab74ea79ce2952fde5fe08d2c268a913025ca
xlsilvaaa <giselesilva080792@gmail.com>
2023-03-04 16:36:21 -0300

    arquivos dos exercicios plantao

---
+++ anoCopa.js
@@ -0,0 +1,5 @@
+//  QUESTÃO 01 - QUAIS OS ANOS DAS COPAS DO MUNDO?
+//escrevendo algoritmo = pesquisar qual o ano da primeira copa? 1930
+//qual o ano da próxima copa do mundo? 2026
+// 
+//
---
+++ segundoPlantao.js
@@ -0,0 +1,4 @@
+//exercício 1 - print na conversa do zap
+
+//exercício 2 - LAÇOS DE REPETIÇÃO - FOR
+
