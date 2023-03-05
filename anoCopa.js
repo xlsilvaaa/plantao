c680d05dc73c1d421ecb2031a0ec5228042a6b3a
xlsilvaaa <giselesilva080792@gmail.com>
2023-03-04 21:42:37 -0300

    atualizando todo os exercicios do plantao

--- anoCopa.js
+++ anoCopa.js
@@ -2,4 +2,14 @@
 //escrevendo algoritmo = pesquisar qual o ano da primeira copa? 1930
 //qual o ano da próxima copa do mundo? 2026
 // 
+/*let anoCopa = 1930;
+//Esta variável inicializa com o número 1930, e toda vez que o laço de repetição rodar, ela recebe um novo valor.
+let ultimaCopaAno = 2026;
+//Esta variável é o limite que defini para o programa rodar.
+
+while (anoCopa <= ultimaCopaAno) {
+    console.log(`Teve copa em: ${anoCopa}`)
+    anoCopa = anoCopa + 4
+}*/
+
 
--- segundoPlantao.js
+++ segundoPlantao.js
@@ -2,3 +2,15 @@
 
 //exercício 2 - LAÇOS DE REPETIÇÃO - FOR
 
+/*const notaProva1 = 5;
+const notaProva2 = 4;
+const trabalhoExtra = 5;
+
+let media = (notaProva1 + notaProva2 + trabalhoExtra) / 3;
+if (media >= 7) {
+    console.log("Parabéns, você passou com média: " + media); 
+} else if (media > 5 && media <7 ) { 
+    console.log("Você ficou de recuperação, com média: " + media);
+} else {
+    console.log(`Reprovado, com média: ${media}`);
+}*/
